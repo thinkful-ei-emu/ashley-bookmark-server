c22b1e863da02b29fe5f76c5fa2d24495849a20c
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-24 20:22:54 -0400

    set up migration files

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, rating, description)
+values
+  ('Facebook', 'http://www.facebook.com', 3, 'connect with friends'),
+  ('Google', 'http://www.google.com', 4, 'search stuff'),
+  ('Fun', 'http://www.fun.com', 1, 'lots of fun'),
+  ('Thinkful', 'http://www.thinkful.com', 2, 'coding bootcamp'),
+  ('Recipes', 'http://www.recipes.com', 3, 'search recipes'),
+  ('Amazon', 'http://www.amazon.com', 5, 'search and buy stuff'),
+  ('ESPN', 'http://www.espn.com', 4, 'sports news'),
+  ('ABC', 'http://www.abc.com', 3, 'watch tv shows'),
+  ('Netflix', 'http://www.netflix.com', 5, 'watch favorite tv and movies'),
+  ('Weather', 'http://www.weather.com', 1, 'get the weather in your area');
--- src/app.js
+++ src/app.js
@@ -27,15 +27,15 @@ app.use(function validateBearerToken(req, res, next) {
 
 
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } };
-  } else {
-    console.error(error);
-    response = { message: error.message, error };
-  }
-  res.status(500).json(response);
-});
+// app.use(function errorHandler(error, req, res, next) {
+//   let response;
+//   if (NODE_ENV === 'production') {
+//     response = { error: { message: 'server error' } };
+//   } else {
+//     console.error(error);
+//     response = { message: error.message, error };
+//   }
+//   res.status(500).json(response);
+// });
 
 module.exports = app;
