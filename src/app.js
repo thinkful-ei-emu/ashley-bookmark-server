9232c1c756e49543de276ff19fdfbf0c2610dd90
Ashley Oelbaum <ashleyoelbaum@gmail.com>
2019-07-24 21:20:30 -0400

    currently unable to aquire a connection for tests

--- src/app.js
+++ src/app.js
@@ -6,36 +6,17 @@ const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const bookmarksRouter = require('./bookmarks/bookmarks.router');
 const app = express();
+const validateBearerToken = require('./validate-bearer-token');
+const errorHandler = require('./error-handler');
 
-const morganOptions = NODE_ENV === 'production' ? 'tiny' : 'common';
-app.use(morgan(morganOptions));
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}));
 app.use(cors());
 app.use(helmet());
+app.use(validateBearerToken);
 app.use('/bookmarks', bookmarksRouter);
+app.use(errorHandler);
 
-
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
-  const authToken = req.get('Authorization');
-
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  //move to the next middleware
-  next();
-});
-
-
-
-// app.use(function errorHandler(error, req, res, next) {
-//   let response;
-//   if (NODE_ENV === 'production') {
-//     response = { error: { message: 'server error' } };
-//   } else {
-//     console.error(error);
-//     response = { message: error.message, error };
-//   }
-//   res.status(500).json(response);
-// });
 
 module.exports = app;
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,9 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+
+require('dotenv').config();
 
 global.expect = expect;
 global.supertest = supertest;
